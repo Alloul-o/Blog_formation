636da596371216724076f73b2a2022c22bfe974a
othmane <alloul.othmane.07@gmail.com>
2024-03-29 09:36:38 +0000

    last commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,45 +9,18 @@ use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
-     /**
-     * @OA\Info(
-     *     title="Article API",
-     *     version="1.0.0"
-     * )
-     */
-    /**@OA\Get(
-            *     path="/articles",
-            *     summary="Get a list of article",
-            *     tags={"Articles"},
-            *     @OA\Response(response=200, description="Successful operation"),
-            *     @OA\Response(response=400, description="Invalid request")
-            * ) */
+     
         public function index()
         {
             return Article::all();
         }
-      /**@OA\Get(
-            *     path="/articles/{article}",
-            *     summary="Get one article",
-            *     tags={"Articles"},
-            *     @OA\Response(response=200, description="Successful operation"),
-            *     @OA\Response(response=400, description="Invalid request")
-            * )
-            */
+      
         public function show($id)
         {
             return Article::find($id);
         }
 
-    /** @OA\Post(
-            *     path="/articles",
-            *     summary="add article",
-            *     tags={"Articles"},
-            *     @OA\Response(response=200, description="Successful operation"),
-            *     @OA\Response(response=400, description="Invalid request")
-            * 
-            *)
-     */
+    
         public function store(Request $request)
         {
             $validator = Validator::make($request->all(), [
@@ -65,13 +38,7 @@ class ArticleController extends Controller
             ]);
             return  response()->json($comment, 200, );
         }
-        /**   @OA\Put(
-            *     path="/articles/{article}",
-            *     summary="update article",
-            *     tags={"Articles"},
-            *     @OA\Response(response=200, description="Successful operation"),
-            *     @OA\Response(response=400, description="Invalid request")
-            * ) */
+        
         public function update(Request $request, $id)
         {
             $validator = Validator::make($request->all(), [
@@ -87,13 +54,7 @@ class ArticleController extends Controller
     
             return $article;
         }
-   /** @OA\delete(
-            *     path="/articles/{article}",
-            *     summary="delete article",
-            *     tags={"Articles"},
-            *     @OA\Response(response=200, description="Successful operation"),
-            *     @OA\Response(response=400, description="Invalid request")
-            * ) */
+   
         public function delete(Request $request, $id)
         {
             $article = Article::findOrFail($id);
